28f3fb8b3f197cfe838d0ea9d21753e7d7fd4d2f
Duc Nguyen <minhduc140583@gmail.com>
2026-09-20 17:00:33 +0700

    Fix settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -52,8 +52,11 @@ export default async function SettingsForm() {
             api="/api/settings"
             confirmMessage={resource.msg_confirm_save}
             successMessage={resource.msg_save_success}
+            forbiddenError={resource.error_403}
             parsingError={resource.error_response_body}
             networkError={resource.error_network}
+            conflictError={resource.error_409}
+            goneError={resource.error_410}
           >
             {resource.submit}
           </SubmitButton>
